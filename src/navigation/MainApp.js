85785daaf2a88fb87f13c410bae7e85ccdff42b2
fachalik <fachalik@gmail.com>
2021-12-27 01:47:03 +0800

    updateRefreshToken and GetUser done

--- src/navigation/MainApp.js
+++ src/navigation/MainApp.js
@@ -8,21 +8,7 @@ import {useSelector} from 'react-redux';
 const MainApp = () => {
   const [isLogin, setIsLogin] = useState(null);
   const authStore = useSelector(state => state.auth.userToken.result);
-  const getData = async () => {
-    try {
-      const value = await AsyncStorage.getItem('userToken');
-      if (value !== null) {
-        // value previously stored
-        setIsLogin(value);
-      }
-    } catch (e) {
-      // error reading value
-      console.log(e);
-    }
-  };
-  useEffect(() => {
-    getData();
-  }, []);
+
   return <>{!!authStore && authStore.access ? <MainStack /> : <AuthStack />}</>;
 };
 
